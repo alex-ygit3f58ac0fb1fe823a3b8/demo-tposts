0f7c41a9b23cc4b7cf0513bb0c0f7da6e1c2a5ff
Alex <alex@dd-sites.com>
2019-03-04 20:12:02 +0200

    Composer project init

---
+++ resources/views/index.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.layout')
+
+@section('header-title', 'Все записи')
+@section('header-title-description', '')
+
+@section('content')
+<!-- Begin Author Posts
+	================================================== -->
+	<div class="graybg authorpage">
+		<div class="container">
+			<div class="listrecent listrelated">
+
+			</div>
+		</div>
+	</div>
+	<!-- End Author Posts
+================================================== -->
+<br>
+@endsection
